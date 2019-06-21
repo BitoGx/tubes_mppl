318bc8009da91827a28d41f389b29e6232e27f91
manajemen-keuangan <46679882+manajemen-keuangan@users.noreply.github.com>
2019-06-21 19:54:39 +0700

    change
    ubah teknisi_kelola ke teknisi_daftar, tambah catatan.php, dan edit UI

--- barang.php
+++ barang.php
@@ -1,8 +1,34 @@
 <html>
 <head>
   <title> Barang </title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Halaman Admin</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="css/skins/_all-skins.min.css">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="bower_components/morris.js/morris.css">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <script language="javascript">
-    function FormValidation() 
+    function FormValidation()
     {
       //Menyimpan nilai variable Object kedalam Variabel biasa dan mengecek agar total barang sesuai
       var Total = document.getElementById("Total");
@@ -24,41 +50,41 @@
       {
         return true;
       }
-    }      
+    }
   </script>
 </head>
 <body>
 <center>
   <form action="php/kelola_barang.php" name="Edit" onsubmit="return FormValidation()"  method="post">
-  <table border="1">
+  <table class="table table-stripe" style="width:50%">
   <?php
-    
+
     //Session Start
     session_start();
-    
+
     if(isset($_POST['IdBarang']))
     {
-      
+
       //Menyimpan Variabel yang di kirim menggunakan method POST
       $IdBarang=$_POST['IdBarang'];
       $_SESSION['IdBarang']=$IdBarang;
       $_SESSION['Control'] = "true";
-      
+
       //Memanggil Connection.php
       require_once "php/connection.php";
-      
+
       //Memilih database
       mysqli_select_db($conn,"dellaria");
-  
+
       //Mempersiapkan Command Query  untuk mengambil data IdBarang,NamaBarang,Jumlah,Baik,Maintenance,Rusak berdasarkan IdBarang
       $sql="select b.IdBarang,b.NamaBarang,b.Jumlah,sb.Baik,sb.Maintenance,sb.Rusak from barang as b,status_barang as sb where b.IdBarang = sb.IdBarang and b.IdBarang=$IdBarang";
-  
+
       //Menjalankan perintah query dan menyimpannya dalam variabel hasil
       $hasil=mysqli_query ($conn,$sql);
-  
+
       //Mengambil 1 baris hasil dari perintah query
       $row=mysqli_fetch_row($hasil);
-      
+
       if($row)
       {
         list($IdBarang,$NamaBarang,$Jumlah,$Baik,$Maintenance,$Rusak)=$row;
--- kelola_akun_teknisi.php
+++ kelola_akun_teknisi.php
@@ -96,26 +96,12 @@
           <ul class="nav navbar-nav">
             <li class="dropdown user user-menu">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <img src="" class="user-image" alt="User Image">
-                <span class="hidden-xs"><?php echo $nama; ?></span>
+                <span class="hidden-xs">Nama : <?php echo $nama; ?></span>
               </a>
-              <ul class="dropdown-menu">
-                <!-- User image -->
-                <li class="user-header">
-                  <img src="" class="img-circle" alt="User Image">
-                  <p>
-                    <?php
-                      echo $nama;
-                    ?>
-                  </p>
-                </li>
                 <!-- Menu Footer-->
                 <li class="user-footer">
-                  <div class="pull-right">
-                    <a href="php/session_logout.php" class="btn btn-default btn-flat">Keluar</a>
-                  </div>
+                    <a href="php/session_logout.php">Keluar</a>
                 </li>
-              </ul>
             </li>
           </ul>
         </div>
@@ -126,24 +112,7 @@
       <!-- sidebar: style can be found in sidebar.less -->
       <section class="sidebar">
         <!-- Sidebar user panel -->
-        <div class="user-panel">
-          <div class="pull-left image">
-            <img src="" class="img-circle" alt="User Image">
-          </div>
-          <div class="pull-left info">
-            <p><?php echo $nama; ?></p>
-          </div>
-        </div>
         <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-          <div class="input-group">
-            <input type="text" name="q" class="form-control" placeholder="Search...">
-            <span class="input-group-btn">
-              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-              </button>
-            </span>
-          </div>
-        </form>
         <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
@@ -157,12 +126,12 @@
               </span>
             </a>
             <ul class="treeview-menu">
-              <li><a href="index_teknisi.php"><i class="fa fa-circle-o"></i>Atur barang</a></li>
-              <li><a href="teknisi_kelola_barang.php"><i class="fa fa-circle-o"></i>Kelola Barang</a></li>
+              <li><a href="index_teknisi.php"><i class="fa fa-circle-o"></i>Detail barang</a></li>
+              <li><a href="teknisi_daftar_barang.php"><i class="fa fa-circle-o"></i>Daftar Barang</a></li>
             </ul>
           </li>
           <li>
-            <a href="">
+            <a href="catatan.php">
               <i class="fa fa-circle-o-notch"></i>
               <span>Catatan</span>
             </a>
@@ -184,17 +153,18 @@
     </aside>
 
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+    <div class="content-wrapper" align="center">
       <!-- Content Header (Page header) -->
       <section class="content-header">
-        <h1>
+        <h1 align="center">
           Kelola Akun
         </h1>
+        <br>
       </section>
       <!-- Main content -->
       <!-- /.content -->
       <form action="php/ganti_password.php" method="post" onsubmit='return PassCheck()'>
-        <table border="1">
+        <table class="table table-stripe" style="width:50%">
           <tr>
             <td>Password Lama : </td>
             <td><input type="password" name="passlama" id="passlama" required> </td>
